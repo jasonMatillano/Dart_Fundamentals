e61e3205c9854d7e849d37e20d4da02bc2653335
Jason <jason.matillano@gmail.com>
2023-08-14 11:50:41 +0800

    varables example

--- test.dart
+++ test.dart
@@ -1,3 +1,16 @@
 void main () {
-    print("Hello");
+    String name = 'Jason';
+    print(name);
+    bool isMale = true;
+    print(isMale);
+    int age = 25;
+    print(age);
+    double height = 1.80;
+    print(height);
+    print('Hello World!');
+    print('Hello $name!');
+    print('Hello $name, you are $age years old.');
+    print('Hello $name, you are $age years old, and your height is $height meters.');
+    print('Hello $name, you are $age years old, and your height is $height meters, and you are $isMale.');
+    print('Hello $name, you are $age years old, and your height is $height meters, and you are $isMale, and your name is $name.');
 }
