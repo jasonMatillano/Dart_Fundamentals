53a4af7b5ac8b45c3c94af555164036dd726482e
Jason <jason.matillano@gmail.com>
2023-08-14 11:53:40 +0800

    0.0.3.Dynamic

--- test.dart
+++ test.dart
@@ -1,16 +1,19 @@
 void main () {
-    String name = 'Jason';
-    print(name);
-    bool isMale = true;
-    print(isMale);
-    int age = 25;
-    print(age);
-    double height = 1.80;
-    print(height);
-    print('Hello World!');
-    print('Hello $name!');
-    print('Hello $name, you are $age years old.');
-    print('Hello $name, you are $age years old, and your height is $height meters.');
-    print('Hello $name, you are $age years old, and your height is $height meters, and you are $isMale.');
-    print('Hello $name, you are $age years old, and your height is $height meters, and you are $isMale, and your name is $name.');
+    
+    dynamic name2 = 'Jason';
+    print(name2);
+    name2 = 25;
+    print(name2);
+    name2 = 1.80;
+    print(name2);
+    name2 = true;
+    print(name2);
+    name2 = 'Hello World!';
+    print(name2);
+    name2 = 'Hello $name2!';
+    print(name2);
+    
+
+
+    
 }
