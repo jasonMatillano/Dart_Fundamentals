141f21e551e7cf7680731fec9b673d2f987e4597
Jason <jason.matillano@gmail.com>
2023-08-14 12:01:27 +0800

    0.0.4.Functions

--- test.dart
+++ test.dart
@@ -1,19 +1,19 @@
 void main () {
-    
-    dynamic name2 = 'Jason';
-    print(name2);
-    name2 = 25;
-    print(name2);
-    name2 = 1.80;
-    print(name2);
-    name2 = true;
-    print(name2);
-    name2 = 'Hello World!';
-    print(name2);
-    name2 = 'Hello $name2!';
-    print(name2);
-    
+    print('object');
+    String greet = greeting();
+    print(greet);
+    String name = namef();
+    print(name);
+    int age = agef();
+    print(age);
+}
 
+String greeting() {
+    return 'Hello';
+}
 
-    
+String namef() => 'John'; //same as return 'John';
+
+int agef() {
+    return 25;
 }
